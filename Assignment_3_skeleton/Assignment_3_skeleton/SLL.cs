ba818e177b297fb8875607cb24ae5f01d4de9f15
wasim822 <wasimborhot@gmail.com>
2025-08-06 13:31:47 -0600

    Add project files.

---
+++ Assignment_3_skeleton/Assignment_3_skeleton/SLL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_skeleton
+{
+    public class SLL : LinkedListADT
+    {
+        public void Append(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(object data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Prepend(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(object data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Retrieve(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
